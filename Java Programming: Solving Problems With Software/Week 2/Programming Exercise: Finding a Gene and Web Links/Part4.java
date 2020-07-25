a2485a2b7db89d765e86b567bcff3562541fbcb8
Kanishk Rathod <68466845+rathodkanishk@users.noreply.github.com>
2020-07-26 03:45:59 +0530

    Update Part4.java

--- Java Programming: Solving Problems With Software/Week 2/Programming Exercise: Finding a Gene and Web Links/Part4.java
+++ Java Programming: Solving Problems With Software/Week 2/Programming Exercise: Finding a Gene and Web Links/Part4.java
@@ -11,17 +11,17 @@ public class Part4 {
     public void getLinks() {
     URLResource ur = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
     // Read the file in the link word by word
-    for (String s : ur.words()) {
-        // Convert string s to lower case
-        String lowerCase = s.toLowerCase();
-        // Check to see if youtube.com is in it
-        if (lowerCase.contains("youtube.com")) {
-            // If yes, find the double quote to the left and right of youtube.com
-            int startIndex = s.indexOf("\"");
-            int stopIndex = s.lastIndexOf("\"");
-            // Print everything between the double quotes
-            System.out.println(s.substring(startIndex + 1, stopIndex));
+        for (String s : ur.words()) {
+            // Convert string s to lower case
+            String lowerCase = s.toLowerCase();
+                // Check to see if youtube.com is in it
+                if (lowerCase.contains("youtube.com")) {
+                    // If yes, find the double quote to the left and right of youtube.com
+                    int startIndex = s.indexOf("\"");
+                    int stopIndex = s.lastIndexOf("\"");
+                    // Print everything between the double quotes
+                    System.out.println(s.substring(startIndex + 1, stopIndex));
+                }
         }
     }
-}
 }
